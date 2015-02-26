400a83ddef415e28a037a882a5f47e571c032df0
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2015-02-26 22:15:12 +0100

    Spend coins

--- src/main/scala/Antike.scala
+++ src/main/scala/Antike.scala
@@ -1,49 +1,62 @@
 import monocle.macros.Lenses
 import monocle.Lens
 
+import State._
+import Ressource._
+
 object Antike extends App {
-  val moves: List[Move] = List(
-    Marble,
-    Temple,
-    Marble,
-    Gold,
-    Temple)
+  val moves: List[Move] = List(Marble, Temple, Marble, Gold, Temple)
+  val initRessouce = Ressource(amount = 3, cities = 1, temples = 0)
+  val initState = State(0, 0, 0, initRessouce, initRessouce, initRessouce)
   
-  val initRessouce = SubState(res = 3, cities = 1, temples = 0)
-  val initState = State(0, 0, initRessouce, initRessouce, initRessouce)
-  val states = moves.scanLeft(initState)((state, move) => move f state)
-  val game = moves.zip(states.tail).zipWithIndex.map {
-    case ((move, state), turn) => s"Turn ${turn + 1}: $move\t($state)" }
-  println(game mkString "\n")
+  val states = moves.scanLeft(initState)((state, move) => move m state)
+  
+  println(showGame(moves, states))
+  
+  def showGame(moves: List[Move], states: List[State]): String = {
+    moves zip states.tail map { case (m, s) => m + "\t" + showState(s) } mkString "\n"
+  }
+
+  def showState(s: State): String = { import s._
+    s"""
+      ${marble.amount},${iron.amount},${gold.amount}:$coins
+      ${actives + idles}A
+      ${marble.temples + iron.temples + gold.temples}T
+      ${marble.cities + iron.cities + gold.cities}C
+    """ split "\n" map (_.trim) mkString " "
+  }
 }
+
+// State
 
 @Lenses
-case class State(actives: Int, idles: Int, marbleSS: SubState, ironSS: SubState, goldSS: SubState) {
-  override def toString: String = s"""
-    ${marbleSS.res},${ironSS.res},${goldSS.res} ${actives + idles}A
-    ${marbleSS.temples + ironSS.temples + goldSS.temples}T
-    ${marbleSS.cities + ironSS.cities + goldSS.cities}C
-  """.split("\n").map(_.trim).mkString(" ").trim
-}
+case class State(actives: Int, idles: Int, coins: Int, marble: Ressource, iron: Ressource, gold: Ressource)
 
-case class SubState(res: Int, cities: Int, temples: Int)
+@Lenses
+case class Ressource(amount: Int, cities: Int, temples: Int)
 
-class Move(val f: State => State)
 
-class GetRessource(l: Lens[State, SubState]) extends Move(
-  l.modify { rs => import rs._
-    copy(res = res + cities + 3 * temples + 1)
-  }
+// Actions
+
+sealed abstract class Move(val m: State => State)
+
+abstract class Collect(lens: Lens[State, Ressource]) extends Move(
+  lens.modify(rs => amount.modify(_ + rs.cities + 3 * rs.temples)(rs)) andThen coins.modify(_ + 1)
 )
-case object Iron extends GetRessource(State.ironSS)
-case object Gold extends GetRessource(State.goldSS)
-case object Marble extends GetRessource(State.marbleSS)
+
+abstract class Spend(price: Int, ressource: Lens[State, Ressource], product: Lens[State, Int]) extends Move({s =>
+  val ressources: Int = ressource composeLens amount get s
+  val spending: Int = ((ressources + s.coins) / price) * price
+  val spendRessources: State => State  = (ressource composeLens amount).modify(_ - (ressources min spending))
+  val spendCoins: State => State  = coins.modify(_ - (0 max (spending - ressources)))
+  val produce: State => State  = product.modify(_ + (ressources + s.coins) / price)
+  spendRessources andThen spendCoins andThen produce apply s
+})
+
+case object Marble extends Collect(marble)
+case object Iron extends Collect(iron)
+case object Gold extends Collect(gold)
 case object Science extends Move(identity)
-case object Move extends Move(s => s.copy(actives = s.actives + s.idles, idles = 0))
-case object Temple extends Move(State.marbleSS.modify { ms => import ms._
-  val newTemples = (res / 5) min cities
-  copy(temples = temples + newTemples, res = res - newTemples * 5)
-})
-case object Army extends Move ({ s => import s._
-  copy(idles = idles + ironSS.res / 2, ironSS = ironSS.copy(res = ironSS.res % 2))
-})
+case object Move extends Move(s => actives.modify(_ + s.idles) andThen idles.set(0) apply s)
+case object Temple extends Spend(5, marble, marble composeLens temples)
+case object Army extends Spend(2, iron, idles)
